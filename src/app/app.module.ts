7673100e4803bd83defef8f94aae25e8b07a8930
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-02-11 10:51:21 -0600

    modulo que agrupa los componentes para auth y pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
+
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { NotfoundComponent } from './pages/notfound/notfound.component';
+
+
+
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
     HeaderComponent,
     FooterComponent,
-    BreadcrumbsComponent,
-    ContactComponent,
-    DashboardComponent,
-    RegisterComponent,
-    LoginComponent,
-    NotfoundComponent
+    BreadcrumbsComponent
   ],
   imports: [
+    PagesModule,
+    AuthModule,
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
