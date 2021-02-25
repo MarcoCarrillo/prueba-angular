bc4294883e7151d6201949c8968f9901e5c584e1
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-02-25 10:50:15 -0600

    Imprimir usuarios en /promesa

--- src/app/pages/promesa/promesa.component.ts
+++ src/app/pages/promesa/promesa.component.ts
@@ -7,39 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromesaComponent implements OnInit {
 
+  usuarios:any;
+  
+
   constructor() { }
 
   ngOnInit(): void {
 
-    const promesa = new Promise((resolve, reject) => {
-      // console.log('Esta es una nueva promesa');
+    // const promesa = new Promise((resolve, reject) => {
+    //   // console.log('Esta es una nueva promesa');
 
-      if(true){
-        resolve('Esta es una nueva promesa');
-      }
-      else{
-        reject('Ocurrio un error');
-      }
+    //   if(true){
+    //     resolve('Esta es una nueva promesa');
+    //   }
+    //   else{
+    //     reject('Ocurrio un error');
+    //   }
 
       
-    });
+    // });
     
-    promesa.then((datos) => {
-      console.log(datos);
-    });
-    promesa.catch((datos) => {
-      console.log(datos);
-    });
+    // promesa.then((datos) => {
+    //   console.log(datos);
+    // });
+    // promesa.catch((datos) => {
+    //   console.log(datos);
+    // });
 
-    console.log('Fin del evento ngOnInit');
+    // console.log('Fin del evento ngOnInit');
 
-    this.obtenerUsuarios().then(usuarios => console.log(usuarios));
+    this.obtenerUsuarios().then(datosUsuarios => {
+      this.usuarios = datosUsuarios;
+      console.log(this.usuarios);
+    });
   }
 
   obtenerUsuarios() {
     const promesa = new Promise((resolve, reject) => {
-      const url = `https://reqres.in/api/users`
-      fetch(url).then(res=>res.json()).then(res=>resolve(res.data) )
+     
+      fetch('https://reqres.in/api/users')
+      .then(res=>res.json())
+      .then(res=>resolve(res.data) )
     });
     return promesa;
 
